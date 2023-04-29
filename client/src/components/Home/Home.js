7ea7104fa712d9de4ed34f99356f9b34505a66f3
Ivanzhela <ivanjelad@abv.bg>
2023-04-30 00:51:58 +0300

    Update CSS

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -5,7 +5,7 @@ export const Home = () => {
     return (
         <div className={style.welcome}>
             <div className={style["welcome-text"]}>
-                <h1>Healthy Food</h1>
+                <h2>Healthy Food</h2>
                 <h1 className={style["bold-welcome"]}>Healthy Baby</h1>
                 <p>Your baby is eating “big kid” food now, so it`s time to start looking beyond the baby food aisle. For healthy choices see all</p>
                 <Link to={'/catalog'}>
